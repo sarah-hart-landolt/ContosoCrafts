c501c9777075208c140054fbaf7e3a25c42c74f5
Sarah Hart Landolt <sarah.hart.landolt@gmail.com>
2020-10-19 14:23:36 -0500

    fixed problem

--- ContosoCrafts/Startup.cs
+++ ContosoCrafts/Startup.cs
@@ -62,12 +62,7 @@ namespace ContosoCrafts
                 endpoints.MapControllers();
                 endpoints.MapBlazorHub();
 
-                // endpoints.MapGet("/products", (context) => 
-                // {
-                //     var products = app.ApplicationServices.GetService<JsonFileProductService>().GetProducts();
-                //     var json = JsonSerializer.Serialize<IEnumerable<Product>>(products);
-                //     return context.Response.WriteAsync(json);
-                // });
+             
             });
         }
     }
